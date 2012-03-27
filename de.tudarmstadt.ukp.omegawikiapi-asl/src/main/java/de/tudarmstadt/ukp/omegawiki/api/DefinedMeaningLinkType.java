76939e249788adb37a742d2ee84c8f00906fc930
Zijad Maksuti <zijad.maksuti@gmail.com>
2012-03-27 22:36:43 +0000

    Initial import.

---
+++ de.tudarmstadt.ukp.omegawikiapi-asl/src/main/java/de/tudarmstadt/ukp/omegawiki/api/DefinedMeaningLinkType.java
@@ -0,0 +1,96 @@
+/*******************************************************************************
+ * Copyright 2012
+ * Ubiquitous Knowledge Processing (UKP) Lab
+ * Technische Universität Darmstadt
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+package de.tudarmstadt.ukp.omegawiki.api;
+
+/**
+ * Holds DefinedMeaning relation constants, that are used in OmegaWiki DataBase
+ * @author matuschek
+ *
+ */
+public class DefinedMeaningLinkType {
+
+	public static final int broader_terms=	4;
+	public static final int related_terms=	6;
+	public static final int is_part_of_theme=	3;
+	public static final int narrower_terms	=5;
+	public static final int parent=	545136;
+	public static final int child	=545139;
+	public static final int is_spoken_in=	725316;
+	public static final int capital=	6873;
+	public static final int borders_on	=726586;
+	public static final int flows_through=	726650;
+	public static final int flows_into	=726670;
+	public static final int ligt_aan_de	=726890;
+	public static final int is_written_in=	728048;
+	public static final int is_expressed_by=	728659;
+	public static final int currency	=347472;
+	public static final int official_language=	476516;
+	public static final int demonym=	733003;
+	public static final int edible=	7784;
+	public static final int edible_part	=734712;
+	public static final int antonym=	7574;
+	public static final int operates_on=	741504;
+	public static final int was_formerly_known_as =	744353;
+	public static final int industry=	1922;
+	public static final int active_in=	744605;
+	public static final int is_administered_by=	750683;
+	public static final int ingredient=	479058;
+	public static final int works_in_a	=754150;
+	public static final int located_in	=755407;
+	public static final int is_member_of=	760699;
+	public static final int wordt_gevolgd_door=	764419;
+	public static final int is_played_by=	764518;
+	public static final int partners_with=	764944;
+	public static final int jouer=	485132;
+	public static final int unit_of=	767120;
+	public static final int família	=8152;
+	public static final int is_practiced_by_a=	789884;
+	public static final int sex	=346514;
+	public static final int female=	6392;
+	public static final int male	=6395;
+	public static final int juvenile	=506365;
+	public static final int is_a_breed_of=	805610;
+	public static final int fedele	=699835;
+	public static final int is_an_allotope_of	=826342;
+	public static final int antideeltje	=367908;
+	public static final int phylum=	799635;
+	public static final int order=	797936;
+	public static final int Class=	797986;
+	public static final int is_made_of=	862691;
+	public static final int domain_biology =	885000;
+	public static final int kingdom=	801379;
+	public static final int family =	347856;
+	public static final int etnia=	636110;
+	public static final int Singular=	424630;
+	public static final int is_located_in_the =	974957;
+	public static final int plant_biology	=2836;
+	public static final int moon=	5729;
+	public static final int is_an_ingredient_of =	1043295;
+	public static final int genus=	715773;
+	public static final int former_name =	1049622;
+	public static final int orbits_around=	1071204;
+	public static final int nfinito=	512433;
+	public static final int holonym=	375078;
+	public static final int meronym=	375074;
+	public static final int constellation=	688481;
+	public static final int country	=5671;
+	public static final int county	=782;
+	public static final int idiom	=506438;
+	public static final int species	=3580;
+
+}
---
+++ de.tudarmstadt.ukp.omegawikiapi-asl/src/main/java/de/tudarmstadt/ukp/omegawiki/api/OWLanguage.java
@@ -0,0 +1,346 @@
+/*******************************************************************************
+ * Copyright 2012
+ * Ubiquitous Knowledge Processing (UKP) Lab
+ * Technische Universität Darmstadt
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+package de.tudarmstadt.ukp.omegawiki.api;
+
+/**
+ * Holds Language Constants, that are used in OW DataBase
+ *
+ * @author matuschek
+ *
+ */
+public class OWLanguage {
+	static String[] langs = new String[500];
+	public static final int  Bulgarian=84;
+	public static final int  English=85;
+
+	public static final int  French=86;
+	public static final int  Castilian=87;
+	public static final int  Russian=88;
+	public static final int  Dutch=89;
+	public static final int  Czech=90;
+	public static final int  Swedish=91;
+	public static final int  Slovenian=92;
+	public static final int  Polish=93;
+	public static final int  Portuguese=94;
+	public static final int  Bokmål=95;
+	public static final int  Basque=96;
+	public static final int  Slovak=97;
+	public static final int  Estonian=98;
+	public static final int  Finnish=99;
+	public static final int  Italian=100;
+	public static final int  German=101;
+	//langs[101]="German";
+	public static final int  Hungarian=102;
+	public static final int  Danish=103;
+	public static final int  English_United_States=104;
+	//langs[104]="English United States";
+	public static final int  Greek=105;
+	public static final int  Hebrew=106;
+	public static final int  Mandarin_simplified=107;
+	public static final int  Neapolitan=109;
+	public static final int  Marathi=110;
+	public static final int  Lombard=111;
+	public static final int  Japanese=112;
+	public static final int  Esperanto=113;
+	public static final int  Piedmontese=114;
+	public static final int  Venetian=115;
+	public static final int  Ukrainian=116;
+	public static final int  Catalan=117;
+	public static final int  Ewe=118;
+	public static final int  Akan=119;
+	public static final int  Arabic=120;
+	public static final int  Hausa_Ajami=121;
+	public static final int  Hausa_Latin=122;
+	public static final int  Armenian=123;
+	public static final int  Georgian=124;
+	public static final int  Afrikaans=125;
+	public static final int  French_Canada=126;
+	public static final int  French_Switzerland=127;
+	public static final int  Serbian_Cyrillic_script=128;
+	public static final int  Serbian_Latin_script=129;
+	public static final int  Thai=130;
+	public static final int  Ido=131;
+	public static final int  Romanian=133;
+	public static final int  Kölsch=134;
+	public static final int  Mandarin_traditional=135;
+	public static final int  English_United_Kingdom=136;
+	//langs[136]="English United Kingdom";
+	public static final int  Turkish=137;
+	public static final int  Korean=138;
+	public static final int  Khmer=139;
+	public static final int  Sicilian=140;
+	public static final int  Aragonese=141;
+	public static final int  Persian=142;
+	public static final int  Latvian=143;
+	public static final int  Vietnamese=144;
+	public static final int  Swahili=145;
+	public static final int  Bavarian=146;
+	public static final int  Belarusian=147;
+	public static final int  Kashubian=148;
+	public static final int  Croatian=149;
+	public static final int  Lithuanian=150;
+	public static final int  Cantonese=151;
+	public static final int  Tajik=152;
+	public static final int  Welsh=153;
+	public static final int  Bambara=154;
+	public static final int  Portuguese_Brazil=155;
+	public static final int  Portuguese_Portugal=156;
+	public static final int  Telugu=158;
+	public static final int  Gilaki=159;
+	public static final int  Eastern_Mari=160;
+	public static final int  Western_Mari=161;
+	public static final int  Gujarati=162;
+	public static final int  Hindi=163;
+	public static final int  Tamil=164;
+	public static final int  Indonesian=165;
+	public static final int  Irish=166;
+	public static final int  French_Belgium=167;
+	public static final int  Limburgish=168;
+	public static final int  Alemannic_German=169;
+	public static final int  Marwari=170;
+	public static final int  Sranan=171;
+	public static final int  German_Austria=172;
+//	langs[172]="German Austria";
+	public static final int  German_Switzerland=173;
+//	langs[173]="German Switzerland";
+	public static final int  Ancient_Greek=174;
+	public static final int  Mycenaean_Greek=175;
+	public static final int  Cappadocian_Greek=176;
+	public static final int  Tosk=177;
+	public static final int  Gheg=178;
+	public static final int  Arvanitic=179;
+	public static final int  Arbëreshë_Albanian=180;
+	public static final int  Kannada=181;
+	public static final int  Breton=182;
+	public static final int  Frisian=183;
+	public static final int  Navajo=184;
+	public static final int  Bengali=185;
+	public static final int  Urdu=186;
+	public static final int  French_France=187;
+	public static final int  Nepali=188;
+	public static final int  Igbo=189;
+	public static final int  Ladin=190;
+	public static final int  Kazakh=191;
+	public static final int  Walloon=192;
+	public static final int  Icelandic=193;
+	public static final int  Tatar=194;
+	public static final int  Asturian=195;
+	public static final int  Sindhi_Arabic_script=196;
+	public static final int  Sindhi_Gurumukhi_script=197;
+	public static final int  Yoruba=198;
+	public static final int  Galician=199;
+	public static final int  Nynorsk=200;
+	public static final int  Lingala=202;
+	public static final int  Xhosa=203;
+	public static final int  Upper_Sorbian=204;
+	public static final int  Lower_Sorbian=205;
+	public static final int  Tagalog=206;
+	public static final int  Maltese=207;
+	public static final int  Cebuano=208;
+	public static final int  Macedonian=209;
+	public static final int  Interlingua=210;
+	public static final int  Interlingue=211;
+	public static final int  Faroese=213;
+	public static final int  Zulu=214;
+	public static final int  Sanskrit=215;
+	public static final int  Nepal_Bhasa=216;
+	public static final int  Malayalam=217;
+	public static final int  Flemish=218;
+	public static final int  Chickasaw=219;
+	public static final int  Corsican=220;
+	public static final int  Volapük=221;
+	public static final int  Malay=222;
+	public static final int  Kabyle=223;
+	public static final int  Manx=224;
+	public static final int  Ossetian=225;
+	public static final int  Crimean_Tatar=226;
+	public static final int  Bashkir=227;
+	public static final int  Cherokee=228;
+	public static final int  Kyrgyz=229;
+	public static final int  Kiribati=230;
+	public static final int  Javanese=231;
+	public static final int  Extremaduran=232;
+	public static final int  Tetum=233;
+	public static final int  Mirandese=234;
+	public static final int  Emiliano_Romagnolo=235;
+	public static final int  Wolof=236;
+	public static final int  Somali=237;
+	public static final int  Latin=238;
+	public static final int  Novial=239;
+	public static final int  Eastern_Yiddish=240;
+	public static final int  Pitjantjatjara=241;
+	public static final int  Lojban=242;
+	public static final int  Luxembourgish=243;
+	public static final int  Romansch=244;
+	public static final int  Mingrelian=245;
+	public static final int  Maori=246;
+	public static final int  Kara_Kalpak=247;
+	public static final int  Divehi=248;
+	public static final int  Assamese=249;
+	public static final int  Abkhaz=250;
+	public static final int  Kotava=251;
+	public static final int  Sundanese=252;
+	public static final int  Cornish=253;
+	public static final int  Pennsylvania_Deitsch=254;
+	public static final int  Comorian=255;
+	public static final int  Saterfriesisch=256;
+	public static final int  Luganda=257;
+	public static final int  Sinhala=258;
+	public static final int  Bishnupriya=259;
+	public static final int  Central_Kurdish=260;
+	public static final int  Tigrigna=261;
+	public static final int  Scottish_Gaelic=262;
+	public static final int  Old_English=263;
+	public static final int  Dzongkha=264;
+	public static final int  Lingua_Franca_Nova=265;
+	public static final int  Nhengatu=266;
+	public static final int  Silesian=267;
+	public static final int  Megleno_Romanian=268;
+	public static final int  Ingush=269;
+	public static final int  Southern_Balochi=270;
+	public static final int  Erzya=271;
+	public static final int  Lao=272;
+	public static final int  Occitan=273;
+	public static final int  Ligurian=274;
+	public static final int  Burmese=275;
+	public static final int  Amharic=276;
+	public static final int  Moksha=277;
+	public static final int  Ilokano=278;
+	public static final int  Chuvash=279;
+	public static final int  Samoan=280;
+	public static final int  Udmurt=281;
+	public static final int  Tok_Pisin=282;
+	public static final int  Franco_Provençal=283;
+	public static final int  Scots=284;
+	public static final int  Wymysorys=285;
+	public static final int  Hawaiian=286;
+	public static final int  Chechen=287;
+	public static final int  Bosnian=288;
+	public static final int  Tongan=289;
+	public static final int  Bakhtiari=290;
+	public static final int  Pontic=291;
+	public static final int  Egyptian_Arabic=292;
+	public static final int  Picard=293;
+	public static final int  South_Saami=294;
+	public static final int  Võro=295;
+	public static final int  Kiche=296;
+	public static final int  Tabasco_Chontal=297;
+	public static final int  Chol=298;
+	public static final int  Chorti=299;
+	public static final int  Tzeltal=300;
+	public static final int  Tzotzil=301;
+	public static final int  Chicomuceltec=302;
+	public static final int  Huastec=303;
+	public static final int  Chuj=304;
+	public static final int  Tojolabal=305;
+	public static final int  Jakalteko=306;
+	public static final int  Kanjobal=307;
+	public static final int  Akateko=308;
+	public static final int  Mocho=309;
+	public static final int  Awakateko=310;
+	public static final int  Ixil=311;
+	public static final int  Mam=312;
+	public static final int  Tektiteko=313;
+	public static final int  Kekchí=314;
+	public static final int  Poqomam=315;
+	public static final int  Poqomchi=316;
+	public static final int  Kaqchikel=317;
+	public static final int  Achi=318;
+	public static final int  Tzutujil=319;
+	public static final int  Sacapulteco=320;
+	public static final int  Sipacapense=321;
+	public static final int  Uspanteco=322;
+	public static final int  Mopán_Maya=323;
+	public static final int  Lacandon=324;
+	public static final int  Yucatán_Maya=325;
+	public static final int  Middle_High_German=327;
+	public static final int  Old_High_German=328;
+	public static final int  Mongolian=329;
+	public static final int  Aceh=330;
+	public static final int  Rapa_Nui=331;
+	public static final int  Old_French=332;
+	public static final int  Middle_French=333;
+	public static final int  Haitian_Creole_French=334;
+	public static final int  Church_Slavic=335;
+	public static final int  Latgalian=336;
+	public static final int  Aramaic_Official_Hebrew=337;
+	public static final int  Aramaic_Jewish_Palestinian=338;
+	public static final int  Classical_Syriac=339;
+	public static final int  Jewish_Babylonian_Aramaic=340;
+	public static final int  Hiligaynon=341;
+	public static final int  Old_Irish=342;
+	public static final int  Karachay_Balkar=343;
+	public static final int  Réunion_Creole=344;
+	public static final int  Benga=345;
+	public static final int  Punu=346;
+	public static final int  Mpongwe=347;
+	public static final int  Kélé=348;
+	public static final int  Chewa=349;
+	public static final int  Bemba=350;
+	public static final int  Umbundu=351;
+	public static final int  Lunda=352;
+	public static final int  Tonga=353;
+	public static final int  Albanian=354;
+	public static final int  Sesotho=355;
+	public static final int  Kalaallisut=356;
+	public static final int  Northern_Kurdish=357;
+	public static final int  Kabuverdianu=358;
+	public static final int  Ugaritic=359;
+	public static final int  Vlax_Romani=360;
+	public static final int  Classical_Armenian=361;
+	public static final int  Ottoman_Turkish=362;
+	public static final int  Abi=363;
+	public static final int  Araki=364;
+	public static final int  Bislama=365;
+	public static final int  Fon=366;
+	public static final int  Futunan=367;
+	public static final int  International=368;
+	public static final int  Holikachuk=369;
+	public static final int  Uzbek=370;
+	public static final int  Anglo_Norman=371;
+	public static final int  Bainouk_Gunyaamolo=372;
+	public static final int  Greek_Ancient_Egypt=373;
+	public static final int  Malagasy=374;
+	public static final int  Tsimihety_Malagasy=375;
+
+	/**
+	 * Helper function for quickly retrieving languages
+	 *
+	 * @param i language code
+	 * @return name of the language
+	 */
+
+	public static String getName(int i)
+	{
+		langs[85]="English";
+		langs[101]="German";
+
+		langs[104]="English United States";
+
+		langs[136]="English United Kingdom";
+
+
+		langs[172]="German Austria";
+		langs[173]="German Switzerland";
+		return langs[i];
+	}
+
+
+
+
+}
